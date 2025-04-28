d38e2464dae5d783c6a03c1b9a112a1c95aac5e3
ghkdusghd <164862216+ghkdusghd@users.noreply.github.com>
2025-04-28 15:20:55 +0900

    first commit

---
+++ src/test/java/com/study/micro_blog/MicroBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.micro_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
